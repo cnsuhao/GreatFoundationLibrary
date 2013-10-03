7bc7d1277b6bafd24492b4c6a57ac4a11555e453
ndkk <sfido@sina.cn>
2013-10-03 21:38:59 +0800

    LLT 测试工程搭建
    完成基本的LLT测试工程搭建

--- shortestpath/product/source/shortestpath.cpp
+++ shortestpath/product/source/shortestpath.cpp
@@ -1,10 +1,21 @@
-/*-----------------------------------------------------------------------------
-    File: shortestpath.cpp
-    Function: 最短路径查询算法实现
-    Author: 唐衡 sfido@sina.cn
-    Date: 2013-9-30
-    City: Chengdu
------------------------------------------------------------------------------*/
+/******************************************************************************
+
+                  版权所有 (C), 2010-2020, 脑袋空空技术有限公司
+
+ ******************************************************************************
+  文 件 名   : shortestpath.cpp
+  版 本 号   : 初稿
+  作    者   : ndkk
+  生成日期   : 2013年10月3日
+  最近修改   :
+  功能描述   : 最短路径算法实现
+
+  修改历史   :
+  1.日    期   : 2013年10月3日
+    作    者   : ndkk
+    修改内容   : 创建文件
+
+******************************************************************************/
 
 #include <stdio.h>
 #include <memory.h>
@@ -27,42 +38,43 @@ CShortestPath::CShortestPath()
 //析构函数
 CShortestPath::~CShortestPath()
 {
-	InitGrayBlackList();
+    InitGrayBlackList();
 }
 
 //清除灰色和黑色集合内存
 void CShortestPath::InitGrayBlackList()
 {
     //释放内存
-	list<ST_PT *>::iterator posGray = g_lstGray.begin();
-	for (; posGray != g_lstGray.end(); ++posGray)
-	{
-	    if (g_ePt == *(*posGray))
-	    {
-	        //do nothing
-	    }
-	    else
-	    {
-		    delete *posGray;
-		}
-	}
-	g_lstGray.clear();
+    list<ST_PT *>::iterator posGray = g_lstGray.begin();
+    for (; posGray != g_lstGray.end(); ++posGray)
+    {
+        if (g_ePt == *(*posGray))
+        {
+            //do nothing
+        }
+        else
+        {
+            delete *posGray;
+        }
+    }
+    g_lstGray.clear();
 
-	list<ST_PT *>::iterator posBlack = g_lstBlack.begin();
-	for (; posBlack != g_lstBlack.end(); ++posBlack)
-	{
-	    if (g_bPt == *(*posBlack))
-	    {
-	        //do nothing
-	    }
-	    else
-	    {
-	        delete *posBlack;
-	    }		
-	}
-	g_lstBlack.clear();
-	
-	g_setBlack.clear();
+    list<ST_PT *>::iterator posBlack = g_lstBlack.begin();
+    for (; posBlack != g_lstBlack.end(); ++posBlack)
+    {
+        if (g_bPt == *(*posBlack))
+        {
+            //do nothing
+        }
+        else
+        {
+            delete *posBlack;
+        }        
+    }
+    g_lstBlack.clear();
+
+    //清除Set集合
+    g_setBlack.clear();
 }
 
 //设置起点
@@ -107,111 +119,111 @@ int CShortestPath::ST2INT(ST_PT &obj)
 ST_PT CShortestPath::INT2ST(int i)
 {
     ST_PT stObj;
-	stObj.x = i >> 8;
-	stObj.y = i - (stObj.x << 8);
-	return stObj;
+    stObj.x = i >> 8;
+    stObj.y = i - (stObj.x << 8);
+    return stObj;
 }
 
 //是否放到灰色集合中
 bool CShortestPath::IsPushGray(ST_PT &objP)
 {
     if (!IsPointValid(objP))
-	{
-		return false;
-	}
+    {
+        return false;
+    }
 
-	if (0 != g_map[objP.x][objP.y])
-	{
-		return false;
-	}
-	
-	int iTmp = ST2INT(objP);
-	SET_INT::iterator pos = g_setBlack.find(iTmp);
-	if (pos != g_setBlack.end())
-	{
-		return false;
-	}
-	
-	return true; 
+    if (0 != g_map[objP.x][objP.y])
+    {
+        return false;
+    }
+    
+    int iTmp = ST2INT(objP);
+    SET_INT::iterator pos = g_setBlack.find(iTmp);
+    if (pos != g_setBlack.end())
+    {
+        return false;
+    }
+    
+    return true; 
 }
 
 //是否是目标点
 bool CShortestPath::IsDestPt(ST_PT &objP)
 {
-    if (g_ePt == objP)	
-	{
-		return true;
-	}
-	return false;
+    if (g_ePt == objP)    
+    {
+        return true;
+    }
+    return false;
 }
 
 //是否找到目标点
 bool CShortestPath::Search(ST_PT *prevPt, E_DIR eDir)
 {
     ST_PT *tmpPt = new ST_PT;
-	tmpPt->prevPt = prevPt;
-	if (E_DIR_EAST == eDir)
-	{
-		tmpPt->x = prevPt->x;
-		tmpPt->y = prevPt->y + 1;
-	}
-	else if (E_DIR_SOUTH == eDir)
-	{
-		tmpPt->x = prevPt->x + 1;
-		tmpPt->y = prevPt->y;
-	}
-	else
-	{
-		tmpPt->x = prevPt->x - 1;
-		tmpPt->y = prevPt->y;
-	}
+    tmpPt->prevPt = prevPt;
+    if (E_DIR_EAST == eDir)
+    {
+        tmpPt->x = prevPt->x;
+        tmpPt->y = prevPt->y + 1;
+    }
+    else if (E_DIR_SOUTH == eDir)
+    {
+        tmpPt->x = prevPt->x + 1;
+        tmpPt->y = prevPt->y;
+    }
+    else
+    {
+        tmpPt->x = prevPt->x - 1;
+        tmpPt->y = prevPt->y;
+    }
 
-	if (!IsPointValid(*tmpPt))
-	{
-		delete tmpPt;
-		tmpPt = NULL;
-		return false;
-	}
+    if (!IsPointValid(*tmpPt))
+    {
+        delete tmpPt;
+        tmpPt = NULL;
+        return false;
+    }
 
-	if (IsPushGray(*tmpPt))
-	{
-		g_lstObj.push_back(tmpPt);
+    if (IsPushGray(*tmpPt))
+    {
+        g_lstObj.push_back(tmpPt);
 
-		if (IsDestPt(*tmpPt))
-		{			
-			return true;
-		}
-	}
-	else
-	{
-		if (IsDestPt(*tmpPt))
-		{
-			delete tmpPt;
-			tmpPt = NULL;
-			return true;
-		}
+        if (IsDestPt(*tmpPt))
+        {            
+            return true;
+        }
+    }
+    else
+    {
+        if (IsDestPt(*tmpPt))
+        {
+            delete tmpPt;
+            tmpPt = NULL;
+            return true;
+        }
 
-		delete tmpPt;
-		tmpPt = NULL;
-	}
+        delete tmpPt;
+        tmpPt = NULL;
+    }
 
-	return false;
+    return false;
 }
 
 //打印结果
 void CShortestPath::ShowResult()
 {
     printf("===============\n");
-	
-	ST_PT *pDest = &g_bPt;
-	
-	//从起始点开始打印查出的目标路径
-	printf("(%d, %d)\n", pDest->x, pDest->y);
-	while (NULL != pDest->nextPt)
-	{
-		pDest = pDest->nextPt;
-		printf("(%d, %d)\n", pDest->x, pDest->y);		
-	}
+    
+    ST_PT *pDest = &g_bPt;
+    
+    //从起始点开始打印查出的目标路径
+    printf("(%d, %d)\n", pDest->x, pDest->y);
+    while (NULL != pDest->nextPt)
+    {
+        pDest = pDest->nextPt;
+        printf("(%d, %d)\n", pDest->x, pDest->y);        
+    }
 }
 
 //计算最短路径
@@ -221,76 +233,76 @@ void CShortestPath::DoService()
     InitGrayBlackList();
     
     //先将起点放到灰色集合中
-	g_lstGray.push_back(&g_bPt);
-	
-	bool bFind = false;   //是否发现目标点
-	ST_PT *pDest = NULL;  //目标点
-	
-	while (1)
-	{
-	    //遍历灰色列表
-		list<ST_PT *>::iterator pos = g_lstGray.begin();
-		for (; pos != g_lstGray.end(); ++pos)
-		{
-		    //将灰色列表中的元素抽取出来，放入黑色列表中
-			ST_PT *pPt = *pos;
-			g_lstBlack.push_back(pPt);
-		
-		    //为了提高检索的效率，这里采用Set标准库函数
-			int iBlack = ST2INT(*pPt);
-			g_setBlack.insert(iBlack);
+    g_lstGray.push_back(&g_bPt);
+    
+    bool bFind = false;   //是否发现目标点
+    ST_PT *pDest = NULL;  //目标点
+    
+    while (1)
+    {
+        //遍历灰色列表
+        list<ST_PT *>::iterator pos = g_lstGray.begin();
+        for (; pos != g_lstGray.end(); ++pos)
+        {
+            //将灰色列表中的元素抽取出来，放入黑色列表中
+            ST_PT *pPt = *pos;
+            g_lstBlack.push_back(pPt);
+        
+            //为了提高检索的效率，这里采用Set标准库函数
+            int iBlack = ST2INT(*pPt);
+            g_setBlack.insert(iBlack);
 
             //向三个方向寻找，如果发现目标点直接退出
-			if (Search(pPt, E_DIR_EAST)){bFind = true; break;}
-			if (Search(pPt, E_DIR_SOUTH)) {bFind = true; break;}
-			if (Search(pPt, E_DIR_NORTH)) {bFind = true; break;}
-		}
-		
-		//更新灰色列表，因为在寻找过程中会重新计算灰色列表
-		g_lstGray = g_lstObj;
-		g_lstObj.clear();
-		if (bFind)
-		{
-			break;
-		}		
-	}
+            if (Search(pPt, E_DIR_EAST)){bFind = true; break;}
+            if (Search(pPt, E_DIR_SOUTH)) {bFind = true; break;}
+            if (Search(pPt, E_DIR_NORTH)) {bFind = true; break;}
+        }
+        
+        //更新灰色列表，因为在寻找过程中会重新计算灰色列表
+        g_lstGray = g_lstObj;
+        g_lstObj.clear();
+        if (bFind)
+        {
+            break;
+        }        
+    }
 
-	//目标点在临时集合中，找出目标点
-	list<ST_PT *>::iterator pos = g_lstGray.begin();
-	for (; pos != g_lstGray.end(); ++pos)
-	{
-		ST_PT *pTmp = *pos;
-		if (g_ePt == *pTmp)
-		{
-			pDest = pTmp;
-			break;
-		}
-	}
+    //目标点在临时集合中，找出目标点
+    list<ST_PT *>::iterator pos = g_lstGray.begin();
+    for (; pos != g_lstGray.end(); ++pos)
+    {
+        ST_PT *pTmp = *pos;
+        if (g_ePt == *pTmp)
+        {
+            pDest = pTmp;
+            break;
+        }
+    }
 
     //从目标点向回追溯到起始点，并构造链接
-	ST_PT *pSwap = NULL;
-	while (NULL != pDest->prevPt)
-	{
-		pSwap = pDest;
-		pDest = pDest->prevPt;
-		pDest->nextPt = pSwap;
-	}	
+    ST_PT *pSwap = NULL;
+    while (NULL != pDest->prevPt)
+    {
+        pSwap = pDest;
+        pDest = pDest->prevPt;
+        pDest->nextPt = pSwap;
+    }    
 }
 
 //是否是有效点
 bool CShortestPath::IsPointValid(ST_PT &obj)
 {
-	if ((obj.x >= m_maxX) || (obj.x < 0))
-	{
-		return false;
-	}
+    if ((obj.x >= m_maxX) || (obj.x < 0))
+    {
+        return false;
+    }
 
-	if ((obj.y >= m_maxY) || (obj.y < 0))
-	{
-		return false;
-	}
+    if ((obj.y >= m_maxY) || (obj.y < 0))
+    {
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 
---
+++ test/testcase/source/llt_shortestpath.cpp
@@ -0,0 +1,89 @@
+/******************************************************************************
+
+                  版权所有 (C), 2010-2020, 脑袋空空技术有限公司
+
+ ******************************************************************************
+  文 件 名   : llt_shortestpath.cpp
+  版 本 号   : 初稿
+  作    者   : ndkk
+  生成日期   : 2013年10月3日
+  最近修改   :
+  功能描述   : 最短路径算法测试类
+  函数列表   :
+              TEST
+  修改历史   :
+  1.日    期   : 2013年10月3日
+    作    者   : ndkk
+    修改内容   : 创建文件
+
+******************************************************************************/
+
+
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <memory.h>
+#include <set>
+#include <list>
+#include <assert.h>
+using namespace std;
+
+#include "shortestpath.h"
+
+class CShortestPathTestSuite : public testing::Test
+{
+protected:
+    void SetUp() 
+    {
+        m_pShortestPath = new CShortestPath();
+    }
+    
+    void TearDown() 
+    {
+        if (NULL != m_pShortestPath)
+        {
+            delete m_pShortestPath;
+            m_pShortestPath = NULL;
+        }
+    }
+    
+    CShortestPath *m_pShortestPath;    
+};
+
+TEST_F(CShortestPathTestSuite, DoService)
+{
+    static const int in_map[3][10] = 
+    {
+        0,1,0,0,0,0,0,0,0,1,  
+        0,1,0,1,0,1,1,1,0,1,
+        0,0,0,0,1,1,1,1,0,0,    
+    };
+
+    ST_PT bPt(0,0);
+    ST_PT ePt(2,9);
+    m_pShortestPath->SetBeginPoint(bPt);
+    m_pShortestPath->SetEndPoint(ePt);        
+    int **ppMap = new int*[MAX_X];
+    for (int i = 0; i < MAX_X; i++)
+    {        
+        int *pMap = new int[MAX_Y];        
+        for (int j = 0; j < MAX_Y; j++)
+        {
+            pMap[j] = in_map[i][j];            
+        }
+        ppMap[i] = pMap;
+    }
+    
+    m_pShortestPath->SetMap(MAX_X, MAX_Y, ppMap);
+    m_pShortestPath->DoService();
+    m_pShortestPath->ShowResult();
+    
+    //释放内存
+    for (int m = 0; m < MAX_X; m++)
+    {
+        delete[] ppMap[m];
+    }
+    delete[] ppMap;
+
+    EXPECT_EQ(6, 6);
+
+}
